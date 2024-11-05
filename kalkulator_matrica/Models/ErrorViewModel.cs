596e5b5131b5b5569cc2a17b46bd7f2ea6b96f81
lukar <lukar@LUKA>
2024-11-05 20:52:09 +0100

    Add project files.

---
+++ kalkulator_matrica/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace kalkulator_matrica.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
